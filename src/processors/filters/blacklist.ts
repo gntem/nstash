9c5114b7ba096f511282f8427b369425699a3e68
Giorgos Ntemiris <ntemirisgiorgos3@gmail.com>
2019-03-28 14:42:11 +0100

    wip

--- src/processors/filters/blacklist.ts
+++ src/processors/filters/blacklist.ts
@@ -1,15 +1,14 @@
 import * as joi from 'joi';
-import { Stream } from 'stream';
+import { Transform, TransformCallback } from 'stream';
 
-import nstashevent from '../../interfaces/NstashEvent';
-import processor from '../../interfaces/BaseProcessor';
+import NstashEvent from '../../interfaces/NstashEvent';
 
 const optionsSchema = joi.object().keys({
   field: joi.string().default(''),
   terms: joi.array().items(joi.string()).default([]),
 });
 
-class BlacklistFilter extends Stream.Transform{
+class BlacklistFilter extends Transform{
   private readonly name = 'blacklist';
   private readonly options: { field: string, terms: [string]};
 
@@ -28,7 +27,7 @@ class BlacklistFilter extends Stream.Transform{
   }
 
   // tslint:disable-next-line: function-name
-  transform(event: nstashevent, encoding: BufferEncoding, callback: Function): void {
+  transform(event: NstashEvent, encoding: BufferEncoding, callback: TransformCallback): void {
     const { field: optField, terms: optTerms } = this.options;
     event.fields().forEach((eventField: {name: string, value: any}) => {
       if (eventField.name === optField && Array.isArray(eventField.value)) {
